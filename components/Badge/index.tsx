dd1b8491e520f04948b9398f8d07ae0478bd15e7
carlo moleka <me@carlomoleka.com>
2023-10-08 12:15:45 -0400

    Use Nextjs fonts and fix image paths

--- components/Badge/index.tsx
+++ components/Badge/index.tsx
@@ -2,8 +2,8 @@ import type { BadgeProps } from "@pTypes/uiTypes";
 
 export const Badge = ({ label }: BadgeProps) => {
   return (
-    <span className="inline-block rounded  bg-turquoise/25 px-2 py-2 text-xs font-medium text-turquoise cursor-default">
-      {label}
-    </span>
+    <div className="flex w-full cursor-default items-center justify-center rounded bg-turquoise/25 px-2 py-1 ">
+      <span className="text-xs font-medium text-turquoise">{label}</span>
+    </div>
   );
 };
